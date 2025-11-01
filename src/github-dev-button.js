a45e9c22ffc9048df5c737e2503c09587f48a61b
seognil LC <seognil@gmail.com>
2025-11-01 12:46:16 +0800

    feat: update github editor logic

--- src/github-dev-button.js
+++ src/github-dev-button.js
@@ -1,32 +1,12 @@
 {
-  /** @param {Element} ul */
-  const addDevButton = (ul) => {
-    const BTN_ID = "github-vscode-button";
-    if (ul.querySelector(`#${BTN_ID}`)) return;
-
-    // * ---------------- create button
-    const li = document.createElement("li");
-    li.id = BTN_ID;
-
-    {
-      const a = document.createElement("a");
-      a.classList.add("btn", "btn-sm");
-      a.textContent = "vs.dev";
-      a.href = `https://vscode.dev/github/${document.location.pathname.slice(1)}`;
-      li.append(a);
+  document.addEventListener("keydown", (e) => {
+    if (e.shiftKey && e.code === "Comma") {
+      e.preventDefault();
+      window.open("https://vscode.dev/github" + window.location.pathname, "_blank");
     }
-    {
-      const a = document.createElement("a");
-      a.classList.add("btn", "btn-sm");
-      a.textContent = "gh.dev";
-      a.href = `https://github.dev/${document.location.pathname.slice(1)}`;
-      li.append(a);
-    }
-
-    ul.prepend(li);
-  };
-
-  domObserverAll(".pagehead-actions", (ul) => {
-    addDevButton(ul);
+    // if (e.shiftKey && e.code === "Period") {
+    //   e.preventDefault();
+    //   window.open("https://github.dev" + window.location.pathname, "_blank");
+    // }
   });
 }
